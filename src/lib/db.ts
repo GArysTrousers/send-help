c8b0f64c155311ba38ff35535e50576331cabb4a
garystrousers <ben.m.lee@outlook.com>
2024-10-23 16:09:38 +1100

    [fix] db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,11 @@
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from "$env/static/private";
 import { Sql } from "./sql";
 
 
 export const sql = new Sql({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '123456',
-  database: 'send-help',
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
 })
