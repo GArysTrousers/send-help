8d996108115a9794081326eb98e819d3c442eae7
GArysTrousers <ben.m.lee@outlook.com>
2025-06-17 09:31:06 +1000

    [fix] init db

--- src/db/init-db.ts
+++ src/db/init-db.ts
@@ -1,21 +1,34 @@
-
-import { DB_FILE } from "$env/static/private";
+import { exit } from "node:process";
 import { Sql } from "../lib/sql.ts";
 import { readFileSync, rmSync, accessSync } from "node:fs";
 
 
-console.log("init db");
+console.log("script: init db");
+
+let env = new Map<string, string>();
+readFileSync('./.env').toString().split(/\r\n/).forEach((v) => {
+  let key = v.split('=', 1)[0]
+  let value = v.slice(v.indexOf('=') + 1)
+  env.set(key, value)
+})
+console.log("DB file:", env.get('DB_FILE'));
+const dbFile = env.get('DB_FILE')
+if (dbFile === undefined) {
+  console.log('no DB_FILE var in .env');
+  exit();
+}
+
 
 try {
-  accessSync(DB_FILE)
+  accessSync(dbFile)
   console.log("deleting old db");
-  rmSync(DB_FILE);
+  rmSync(dbFile);
 } catch (e) {
 
 }
 
 console.log("creating new db");
-const db = new Sql(DB_FILE)
+const db = new Sql(dbFile)
 
 console.log("creating tables");
 db.run(readFileSync('./src/db/Tables.sql').toString());
